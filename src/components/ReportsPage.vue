bcea5322f650018d4b8b5b07f93a8b58c71b19ab
Kirill <kerell.arminin@gmail.com>
2023-04-26 13:27:52 +0300

    Create ReportsPage.vue
    Добавил компоненту (пока заглушку) для страницы отчетов

---
+++ src/components/ReportsPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <GlobalHeader />
+    <h1>This is an reports page</h1>
+  </div>
+</template>
+
+<script>
+import GlobalHeader from "./components/GlobalHeader.vue";
+
+export default {
+  name: "ReportsPage",
+  components: {
+    GlobalHeader,
+  },
+};
+</script>
+
+<style scoped>
+.reportsPage {
+  margin-bottom: 40px;
+}
+</style>
