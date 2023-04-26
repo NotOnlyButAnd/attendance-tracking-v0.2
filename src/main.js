8161a058f51c599ad4de6822110fecea142e244f
Kirill <kerell.arminin@gmail.com>
2023-04-26 13:22:06 +0300

    Fix router
    Сделал адекватное представление роутера в отдельном файле. Подключил его к приложению

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
-import Router from "vue-router";
 import App from "./App.vue";
+import router from "./router";
 import store from "./store";
 import "./plugins/bootstrap";
 
 Vue.config.productionTip = false;
-Vue.use(Router);
 
 new Vue({
+  router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
