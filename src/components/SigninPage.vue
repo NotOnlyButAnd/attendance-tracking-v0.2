9237728da2ec0266dfb8cf2516eb62101a2e3d47
Kirill <kerell.arminin@gmail.com>
2023-04-30 12:30:53 +0300

    Add auth logic
    Добавил методы реализующие логику авторизации. Навесил на форму.

--- src/components/SigninPage.vue
+++ src/components/SigninPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-25 mx-auto border p-3 rounded">
-    <b-form>
+    <b-form @submit="login">
       <div class="form-group">
         <label for="username" class="mb-2">Логин:</label>
         <b-input
@@ -34,6 +34,28 @@ export default {
       username: "",
       password: "",
     };
+  },
+  methods: {
+    login(event) {
+      event.preventDefault();
+
+      // логика авторизации
+      this.axios
+        .post(`http://localhost:7000/api/auth/token/`, {
+          username: this.username,
+          password: this.password,
+        })
+        .then((response) => {
+          this.setLogined(response.data.token);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    setLogined(token) {
+      // сохраняем токен
+      console.log(token);
+    },
   },
 };
 </script>
