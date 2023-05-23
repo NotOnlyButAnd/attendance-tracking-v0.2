5d03f936dcf04191ed704d4b4b6f801302966c72
Kirill <kerell.arminin@gmail.com>
2023-05-23 23:38:31 +0300

    UPD apoi url, check page, config
    Обновил адрес api в env-файле, т.к. настроил все на https (кривой ну ладно). Добавил в компоненту проверки на паре все что нужно для скрина (web-морда готова, без точного функционала). Проверил получение координат - окей. Vue config изменил, чтобы приложение запускалось в dev-сборке с сгенеренными сертификатами.

--- src/components/StudentCheckPage.vue
+++ src/components/StudentCheckPage.vue
@@ -1,8 +1,53 @@
 <template>
-  <div>
-    <h1>Страница для авторизации студента на паре</h1>
-  </div>
+  <section class="ui two column centered grid">
+    <!-- <div>
+      <h1>Страница для авторизации студента на паре</h1>
+    </div> -->
+    <!-- <div class="column">
+      <form class="ui segment large form">
+        <div class="field">
+          <div class="ui right icon input large">
+            <input
+              type="text"
+              placeholder="Enter your address"
+              v-model="address"
+              ref="autocomplete"
+            />
+            <b-button @click="locatorButtonPressed"
+              >Получить координаты</b-button
+            >
+          </div>
+        </div>
+      </form>
+    </div> -->
+    <div>
+      <h1>Вы успешно отмечены на занятии!</h1>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      address: "",
+    };
+  },
+  methods: {
+    locatorButtonPressed() {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position.coords.latitude);
+          console.log(position.coords.longitude);
+        },
+        (error) => {
+          console.log(error.message);
+        }
+      );
+    },
+  },
+};
+</script>
 
 <style scoped>
 h1 {
